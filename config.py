d453d9307bb43857ca1c276312048295182bb0aa
Munge Ryan <mungeryan@gmail.com>
2020-10-19 17:01:16 +0300

    category api link

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
 
-    DEBUG = True
+    DEBUG = Tru
 
 config_options = {
     'development':DevConfig,
