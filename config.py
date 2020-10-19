bf927ec47a3b122cabbff07090d521778a6be257
Munge Ryan <mungeryan@gmail.com>
2020-10-19 22:22:36 +0300

    configuring the category url

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    CAT_API_URL = 'https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey={}'
+    CAT_API_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
   
 
 class ProdConfig(Config):
